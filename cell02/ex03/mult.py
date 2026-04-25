16741f7d9ff8021d79fa72c45b5ae3fb0f25bfad
PUN1407 <ppannatat14@gmail.com>
2026-04-25 08:58:28 +0000

    03

---
+++ cell02/ex03/mult.py
@@ -0,0 +1,11 @@
+num = int(input())
+ber = int(input())
+Ans = num * ber
+print(num, "*",ber, "=", num * ber )
+if num * ber < 0 :
+    print("The result is negative.")
+elif num * ber > 0 :
+    print("The result is positive.")
+else :
+    print("The result is positive and negative")
+
